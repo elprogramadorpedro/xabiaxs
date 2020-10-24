f2146272c1d16920c2f44d1fbf7c08a6bb6ed73f
Pedro <elprogramadorpedro@gmail.com>
2020-10-24 10:45:17 -0300

    adicionando funcionalidad criar imagem

--- xabiaxfront/src/components/NewImg/NewImg.js
+++ xabiaxfront/src/components/NewImg/NewImg.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Typography } from "@material-ui/core";
-import { CadastroForm, Background, GlobalStyle, PositionTypo } from "./styles";
-import { withStyles } from "@material-ui/core/styles";
+import {
+  CadastroForm,
+  PositionTypo,
+  DivGaleryCadastroForm,
+  GaleryImages,
+  informacoesImages,
+} from "./styles";
 import AddIcon from "@material-ui/icons/Add";
 import Fab from "@material-ui/core/Fab";
-import DeleteIcon from "@material-ui/icons/Delete";
-import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
+import CardImg from "../CardImg/CardImg";
 
-export function NewImg() {
+export function NewImg(props) {
+  const [subtitle, setSubititle] = useState("");
+
+  const [author, setAuthor] = useState("");
+
+  const [file, setFile] = useState("");
+
+  const [tags, setTags] = useState("");
+
+  const [collection, setCollection] = useState("");
+
   return (
     <div>
-      <GlobalStyle></GlobalStyle>
-      <Background>
+      <DivGaleryCadastroForm>
         <CadastroForm /*onSubmit={onSubmitLogin}*/>
           <PositionTypo>
             <Tooltip title="Add" aria-label="Add">
@@ -26,53 +39,53 @@ export function NewImg() {
             label={"Subtitle"}
             type={"text"}
             variant="outlined"
-            //onChange={onChangeInput}
-            // value={form['email']}
-            //name={'email'}
+            value={subtitle}
+            onChange={(event) => setSubititle(event.target.value)}
           />
           <TextField
             label={"Author"}
             type={"text"}
             variant="outlined"
-            //onChange={onChangeInput}
-            // value={form['email']}
-            //name={'email'}
+            value={author}
+            onChange={(event) => setAuthor(event.target.value)}
           />
+
           <TextField
             label={"File"}
             type={"text"}
             variant="outlined"
-            //onChange={onChangeInput}
-            // value={form['email']}
-            //name={'email'}
+            value={file}
+            onChange={(event) => setFile(event.target.value)}
           />
 
           <TextField
             label={"Tags"}
             type={"text"}
             variant="outlined"
-            //onChange={onChangeInput}
-            // value={form['email']}
-            //name={'email'}
+            value={tags}
+            onChange={(event) => setTags(event.target.value)}
           />
 
           <TextField
             label={"Collection"}
             type={"text"}
             variant="outlined"
-            //onChange={onChangeInput}
-            // value={form['email']}
-            //name={'email'}
+            value={collection}
+            onChange={(event) => setCollection(event.target.value)}
           />
 
           <Button variant={"contained"} color={"primary"} type={"submit"}>
-            Enviar
+            Criar Imagem
           </Button>
           <Button variant={"contained"} color={"secundary"} type={"submit"}>
             Imagens
           </Button>
         </CadastroForm>
-      </Background>
+        <GaleryImages>
+          <CardImg></CardImg>
+          <informacoesImages></informacoesImages>
+        </GaleryImages>
+      </DivGaleryCadastroForm>
     </div>
   );
 }
